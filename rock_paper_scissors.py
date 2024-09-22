d03580e58ccfe2492678a11b8b7c4ebea8e0d2a6
July132 <july.nenov.sf@gmail.com>
2024-09-22 17:54:26 +0300

    Modified code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,38 +1,47 @@
 import random
 
-rock = 'Rock'
-paper = 'Paper'
-scissors = 'Scissors'
+while True:
+    rock = 'Rock'
+    paper = 'Paper'
+    scissors = 'Scissors'
 
-player_move = input('Choose [r]ock, [p]aper, [s]isscors: ')
+    player_move = input('Choose [r]ock, [p]aper, [s]isscors: ')
 
-if player_move == 'r':
-    player_move = rock
-elif player_move == 'p':
-    player_move = paper
-elif player_move == 's':
-    player_move = scissors
-else:
-    raise SystemExit("Invalid input. Try again.")
+    if player_move == 'r':
+        player_move = rock
+    elif player_move == 'p':
+        player_move = paper
+    elif player_move == 's':
+        player_move = scissors
+    else:
+        print("Invalid input. Try again.")
+        continue
 
-computer_choice = random.randint(1, 3)
+    computer_choice = random.randint(1, 3)
 
-computer_move = ""
+    computer_move = ""
 
-if computer_choice == 1:
-    computer_move = rock
-elif computer_choice == 2:
-    computer_move = paper
-else:
-    computer_move = scissors
+    if computer_choice == 1:
+        computer_move = rock
+    elif computer_choice == 2:
+        computer_move = paper
+    else:
+        computer_move = scissors
 
-print(f'The computer chose {computer_move}.')
+    print(f'The computer chose {computer_move}.')
 
-if (player_move == rock and computer_move == scissors) or \
-        (player_move == scissors and computer_move == paper) or \
-        (player_move == paper and computer_move == rock):
-    print('You win!')
-elif computer_move == player_move:
-    print('Draw!')
-else:
-    print('You lose!')
+    if (player_move == rock and computer_move == scissors) or \
+            (player_move == scissors and computer_move == paper) or \
+            (player_move == paper and computer_move == rock):
+        print('You win!')
+    elif computer_move == player_move:
+        print('Draw!')
+    else:
+        print('You lose!')
+
+    restart = input('Play again? Y/N')
+
+    if restart.lower() == 'y':
+        continue
+    else:
+        break
